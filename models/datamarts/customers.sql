63d1c172aadcb680e489ff43b469ae657ce7cd92
Ronak Mishra <ronak619ad@gmail.com>
2025-07-01 04:58:23 +0000

    updted the customer requirements

--- models/datamarts/customers.sql
+++ models/datamarts/customers.sql
@@ -1,3 +1,14 @@
+-- Requirement: Customer Payments Summary Table
+-- The client requested a summary table that showcases key customer-level insights for demo purposes. The goal was to present a simplified use case that highlights DBT’s transformation capabilities using modular SQL logic and CTEs.
+
+-- Specifically, the client wanted:
+-- ->A table listing each customer
+-- ->The total payments made by each customer
+-- ->The date of their first order
+-- ->The date of their most recent order
+-- ->To achieve this, we pulled data from the staging layer (stg_customers, stg_orders, stg_payments) and applied transformations in the data mart layer using CTEs for clarity and easier debugging.
+
+
 with customers as (
         select * from {{ref('stg_customers')}}
 
@@ -35,12 +46,23 @@ payment_details as(
 
 ) ,
 
+-- final_table as (
+--         select cl.*,pd.*
+--         from customer_level_details cl
+--         left join payment_details pd
+--         on cl.CUSTOMER_ID = pd.CUSTOMER_ID
+--         order by cl.CUSTOMER_ID
+-- )
 final_table as (
-        select cl.*,pd.*
-        from customer_level_details cl
-        left join payment_details pd
+    select 
+        cl.CUSTOMER_ID,
+        cl.FIRST_ORDER,
+        cl.MOST_RECENT_ORDER,
+        pd.AMOUNT
+    from customer_level_details cl
+    left join payment_details pd
         on cl.CUSTOMER_ID = pd.CUSTOMER_ID
-        order by cl.CUSTOMER_ID
+    order by cl.CUSTOMER_ID
 )
 
 select * from final_table
